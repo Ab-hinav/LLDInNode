099334b82c8d80d39879ba69b69036aa31ccb5f1
Abhinav Singh <abhinavsingh@Abhinavs-MacBook-Air.local>
2023-10-11 11:34:57 +0530

    added composite pattern

---
+++ compositePattern/dist/index.js
@@ -0,0 +1,9 @@
+import * as readline from 'readline';
+const r1 = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+r1.question('how are you?', (v) => {
+    console.log('me', v);
+    r1.close();
+});
